a45f38b3a3bf4f99b0e4d0201619cf85f4f61e97
Jeff Shen <jshen@flatironinstitute.org>
2025-06-23 14:51:38 -0400

    add trouble.nvim

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,4 +1,41 @@
 return {
+  {
+    "folke/trouble.nvim",
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
+  },
   { 
     'echasnovski/mini.pairs', 
     version = false,
@@ -25,14 +62,6 @@ return {
   },
   {
     "folke/lazydev.nvim",
-    -- ft = "lua", -- only load on lua files
-    -- opts = {
-    --   library = {
-    --     -- See the configuration section for more details
-    --     -- Load luvit types when the `vim.uv` word is found
-    --     { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-    --   },
-    -- },
   },
   {
     "folke/which-key.nvim",
